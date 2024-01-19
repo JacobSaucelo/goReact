bed330974e9b0a01b891c54a6f9a788dc134d614
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-01-19 21:06:51 +0800

    working frontend and backend testing

--- server/main.go
+++ server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type items struct {
+	Name  string `json:name`
+	Price uint8  `json:price`
+}
 
 func main() {
-	fmt.Println("hwllo world")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handleReturnItems)
+
+	http.ListenAndServe(":3333", mux)
+}
+
+func handleReturnItems(w http.ResponseWriter, r *http.Request) {
+
+	var backpack = []items{
+		{
+			Name:  "book",
+			Price: 10,
+		},
+		{
+			Name:  "sword",
+			Price: 8,
+		},
+		{
+			Name:  "bow",
+			Price: 12,
+		},
+	}
+	response, err := json.Marshal(backpack)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(response)
+
 }
