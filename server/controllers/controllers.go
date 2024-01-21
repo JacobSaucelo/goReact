92a146739d21a8528a36a37364a17fa67450722a
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-01-21 10:06:06 +0800

    added getTodoList

--- server/controllers/controllers.go
+++ server/controllers/controllers.go
@@ -1,16 +1,36 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"com.jacobsaucelo.go-react/models"
 )
 
-func GetTodoList() models.TasksCollection {
-	fPath := filepath.Join("/data/", "data.json")
+var folderName = "/data/"
+var saveFileName = "data.json"
 
-	saveFile, err := os.Open()
+func GetTodoList() models.TasksCollection {
+	fPath := filepath.Join(folderName, saveFileName)
+
+	saveFile, err := os.Open(fPath)
+	if err != nil {
+		fmt.Println("SERVER: Error opening file, ", err)
+		return models.TasksCollection{}
+	}
+	defer saveFile.Close()
+
+	var appTodoList models.TasksCollection
+
+	err = json.NewDecoder(saveFile).Decode(&appTodoList)
+	if err != nil {
+		fmt.Println("SERVER: Error decoding savefile, ", err)
+		return models.TasksCollection{}
+	}
+
+	return appTodoList
 }
 
 func NewTodoList() *models.TasksCollection {
