b9af8d303e998cf1130880c7c1adb0d957494ff8
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-01-22 10:06:17 +0800

    added shadcn

--- frontend/src/components/Comp.DisplayTodos.tsx
+++ frontend/src/components/Comp.DisplayTodos.tsx
@@ -1,8 +1,12 @@
 import { TodosType } from "../../types/todos";
+import { ModeToggle } from "./mode-toggle";
 
 export default function CompDisplayTodos({ todos }: { todos: TodosType[] }) {
   return (
     <article>
+      <ModeToggle />
+
+      <h1 className="text-muted-foreground">hwllo</h1>
       {todos.map((todo) => (
         <aside key={todo.ID}>
           <p>{todo.ID}</p>
